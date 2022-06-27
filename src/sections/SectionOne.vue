9645a115b250410fdb93cee18cba777c69bc7d30
str1ve <end3avour21@gmail.com>
2022-06-28 00:16:02 +0700

    Update SectionOne.vue

--- src/sections/SectionOne.vue
+++ src/sections/SectionOne.vue
@@ -1,7 +1,12 @@
 <template>
-  <section class="flex items-center h-screen w-3/4 px-10">
+  <section>
     <ThemeSwitcherVue />
-    <h1 class="text-eva text-6vw">Здравствуйте, меня зовут Михаил.</h1>
+    <div class="bg-section-one grid place-content-center h-screen lg:w-3/4 w-full px-10">
+      <h1 class="text-eva lg:text-6vw text-6vh lg:text-left text-center">
+        Здравствуйте, меня зовут <span class="text-orange-500">Михаил</span>.
+      </h1>
+      <p class="text-raleway lg:text-2vw text-2vh lg:text-left text-center">И я - FrontEnd разработчик.</p>
+    </div>
   </section>
 </template>
 
@@ -9,4 +14,16 @@
 import ThemeSwitcherVue from '../components/ThemeSwitcher.vue';
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.bg-section-one {
+  background: linear-gradient(360deg, #171717 0%, #000000 100%);
+  border-radius: 0px 0px 75px 75px;
+}
+
+@media (min-width: 1024px) {
+  .bg-section-one {
+    background: linear-gradient(270deg, #171717 0%, #000000 100%);
+    border-radius: 0px 75px 75px 0px;
+  }
+}
+</style>
