de767a031544a69138d57f135b4c84413b182875
Michail <end3avour21@gmail.com>
2022-06-29 13:31:59 +0700

    portfolio title

--- src/sections/SectionThree.vue
+++ src/sections/SectionThree.vue
@@ -1,5 +1,7 @@
 <template>
-  <section></section>
+  <section id="section-three">
+    <h1 class="text-eva lg:text-5vw text-4vh text-center">Портфолио.</h1>
+  </section>
 </template>
 
 <script setup></script>
