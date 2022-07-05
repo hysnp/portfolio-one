4ed7f59a6db50259688687f22bc7a37e1dd03aa3
str1ve <end3avour21@gmail.com>
2022-07-05 15:16:36 +0700

    what if i use docs for gh pages

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,13 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/portfolio-one/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  base: "/portfolio-one/",
+  build: {
+    outDir: "docs",
   },
 });
